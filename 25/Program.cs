6db615455536f0f6de13aed50798139376db13fb
Sergey <sergey06062001@gmail.com>
2022-03-28 15:10:47 +0300

    Task 25 corrected

--- 25/Program.cs
+++ 25/Program.cs
@@ -5,12 +5,22 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-double Pow_ab(int num1, int num2)
+int Pow_ab(int a, int b)
 {
-    double num1_d = Convert.ToDouble(num1);
-    double num2_d = Convert.ToDouble(num2);
-    double pow = Math.Pow(num1_d, num2_d);
-    return pow;
+    int a_temp = a;
+    for(int i = 1; i < b; i++)
+    {
+        a = a * a_temp;
+    }
+    return a;
 }
+
+// double Pow_ab(int num1, int num2)
+// {
+//    double num1_d = Convert.ToDouble(num1);
+//    double num2_d = Convert.ToDouble(num2);
+//    double pow = Math.Pow(num1_d, num2_d);
+//    return pow;
+// }
 
 Console.WriteLine($"Число {a} в степени {b} = {Pow_ab(a, b)}");
